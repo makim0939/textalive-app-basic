4fd8b3a6db57739fc9dd984fc8b2cf27d764599f
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-05-31 10:30:53 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import "./App.css";
-import { IPlayerApp, IRenderingUnit, IVideo, Player, Timer } from "textalive-app-api";
+import { IPlayerApp, Player, Timer } from "textalive-app-api";
 
 function App() {
   const mediaRef = useRef<HTMLDivElement>(null);
@@ -8,137 +8,130 @@ function App() {
   const wordDisplayRef = useRef<HTMLDivElement>(null);
   const phraseDisplayRef = useRef<HTMLDivElement>(null);
   const chordDisplayRef = useRef<HTMLDivElement>(null);
-  const beatsDisplayRef = useRef<HTMLDivElement>(null);
+  const beatDisplayRef = useRef<HTMLDivElement>(null);
   const playBtnRef = useRef<HTMLButtonElement>(null);
   const pauseBtnRef = useRef<HTMLButtonElement>(null);
   const rewindBtnRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
-    if (!mediaRef.current) return;
-    if (!(playBtnRef.current && pauseBtnRef.current && rewindBtnRef.current)) return;
-    if (
-      !(
-        charDisplayRef &&
-        wordDisplayRef.current &&
-        phraseDisplayRef &&
-        chordDisplayRef.current &&
-        beatsDisplayRef.current
-      )
-    )
-      return;
-    console.log(playBtnRef.current);
+    const isRefReady =
+      mediaRef.current &&
+      charDisplayRef.current &&
+      wordDisplayRef.current &&
+      phraseDisplayRef.current &&
+      chordDisplayRef.current &&
+      beatDisplayRef.current &&
+      playBtnRef.current &&
+      pauseBtnRef.current &&
+      rewindBtnRef.current;
+    if (!isRefReady) return;
 
-    const player = new Player({
-      app: { token: import.meta.env.APP_TOKEN },
-      mediaElement: mediaRef.current,
-    });
-
+    //ボタンに再生・停止・巻き戻しをセットします。
     const addButtonEventListeners = () => {
       playBtnRef.current!.addEventListener("click", () => player.requestPlay());
       pauseBtnRef.current!.addEventListener("click", () => player.requestPause());
       rewindBtnRef.current!.addEventListener("click", () => player.requestMediaSeek(0));
     };
+
+    // ===== 楽曲情報を取得、表示するためのメソッドを用意 ===== //
+    //Playerに設定するイベントリスナです。(onAppReady, onTimerReady)
     const onAppReady = (app: IPlayerApp) => {
       player.createFromSongUrl("https://piapro.jp/t/fnhJ/20230131212038");
       addButtonEventListeners();
     };
-    const onVideoReady = (v: IVideo) => {
-      // const animateWord = function (now: number, unit: IRenderingUnit) {
-      //   if (unit.contains(now)) {
-      //     wordDisplayRef.current!.textContent = unit.toString();
-      //   }
-      // };
-      // let w = player.video.firstWord;
-      // while (w) {
-      //   console.log(w.text);
-      //   w.animate = animateWord;
-      //   w = w.next;
-      // }
-    };
     const onTimerReady = (t: Timer) => {
-      console.log("timer Ready");
+      //再生の準備が整った時に呼ばれます。
       playBtnRef.current!.disabled = false;
       pauseBtnRef.current!.disabled = false;
       rewindBtnRef.current!.disabled = false;
     };
-    let prevBeatStartTime = -1;
+
+    let prevBeatPosition = -1;
     const onTimeUpdate = (position: number) => {
+      //歌詞の取得。文字・単語・フレーズ単位で取得できます。
       const char = player.video.findChar(position)?.text;
       const word = player.video.findWord(position)?.text;
       const phrase = player.video.findPhrase(position)?.text;
-
+      //歌詞があれば表示します。
       char && (charDisplayRef.current!.textContent = char);
       word && (wordDisplayRef.current!.textContent = word);
       phrase && (phraseDisplayRef.current!.textContent = phrase);
+
+      //コードの取得
       chordDisplayRef.current!.textContent = player.findChord(position).name;
+      //ビートの取得
       const beat = player.findBeat(position);
-      if (beat && beat.startTime !== prevBeatStartTime) {
-        //取得したビートが小節中の何拍目かに応じて「*」を表示します。
-        prevBeatStartTime = beat.startTime;
-        let beatText = "";
-        for (let i = 0; i < beat.position; i++) {
-          beatText += "* ";
-        }
-        beatsDisplayRef.current!.textContent = beatText;
+      if (beat.position === prevBeatPosition) return;
+      let beatText = "";
+      for (let i = 0; i < beat.position; i++) {
+        beatText += "* ";
       }
+      beatDisplayRef.current!.textContent = beatText;
+      prevBeatPosition = beat.position;
     };
 
+    // ===== Playerを作成してリスナを登録することで順次処理を開始 ===== //
+    const player = new Player({
+      app: { token: "pLPdxQ1b4e3Rfn0O" },
+      mediaElement: document.querySelector("#media") as HTMLDivElement,
+    });
     player.addListener({
       onAppReady,
-      onVideoReady,
       onTimerReady,
       onTimeUpdate,
     });
-
-    // // loopId = requestAnimationFrame(loop);
-
     return () => {
       player.dispose();
     };
   }, []);
+
   return (
-    <>
+    <div id="app">
       <h1>TextAlive App Basic</h1>
-      <div>
+      <div className="controls">
         <div id="media" ref={mediaRef}></div>
-        <button ref={playBtnRef} disabled>
-          Play
-        </button>
-        <button ref={pauseBtnRef} disabled>
-          Pause
-        </button>
-        <button ref={rewindBtnRef} disabled>
-          Rewind
-        </button>
+        <div className="buttons">
+          <button ref={playBtnRef} disabled>
+            Play
+          </button>
+          <button ref={pauseBtnRef} disabled>
+            Pause
+          </button>
+          <button ref={rewindBtnRef} disabled>
+            Rewind
+          </button>
+        </div>
       </div>
 
       <div>
         <h3>Lyric</h3>
         <div className="div"></div>
-        <p>
-          Char:
-          <span className="display" ref={charDisplayRef}></span>
-        </p>
-        <p>
-          Word:
-          <span className="display" ref={wordDisplayRef}></span>
-        </p>
-        <p>
-          phrase:
-          <span className="display" ref={phraseDisplayRef}></span>
-        </p>
-        <h3>Chord</h3>
-        <div className="div"></div>
-        <p>
-          <span className="display" ref={chordDisplayRef}></span>
-        </p>
-        <h3>Beats</h3>
-        <div className="div"></div>
-        <p>
-          <span className="display" ref={beatsDisplayRef}></span>
-        </p>
+        <div>
+          <p>
+            Char:
+            <span className="display" ref={charDisplayRef}></span>
+          </p>
+          <p>
+            Word:
+            <span className="display" ref={wordDisplayRef}></span>
+          </p>
+          <p>
+            phrase:
+            <span className="display" ref={phraseDisplayRef}></span>
+          </p>
+          <h3>Chord</h3>
+          <div className="div"></div>
+          <p>
+            <span className="display" ref={chordDisplayRef}></span>
+          </p>
+          <h3>Beats</h3>
+          <div className="div"></div>
+          <p>
+            <span className="display" ref={beatDisplayRef}></span>
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
